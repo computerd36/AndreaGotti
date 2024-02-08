663ba2e40cbee5a88b3107a75352105219008e7b
Deniz Petzold <deniz.petzold@gmail.com>
2024-02-08 15:38:37 +0100

    changed path in RecentPage.tsx

--- src/pages/RecentPage.tsx
+++ src/pages/RecentPage.tsx
@@ -10,7 +10,7 @@ export interface IRecentPageProps {
 export function RecentPage(props: IRecentPageProps) {
   return (
     <div className='page recentPage'>
-      <img draggable={"false"} src="src/assets/images/piano_2023_transparent.png" alt="Painting of a piano" />
+      <img draggable={"false"} src="/images/piano_2023_transparent.png" alt="Painting of a piano" />
     </div>
   );
 }
